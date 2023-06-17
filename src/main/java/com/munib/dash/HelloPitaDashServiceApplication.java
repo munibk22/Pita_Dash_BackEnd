eceae5db748864a1cbfb3700ce6528bb7370676d
munibk22 <munib22@gmail.com>
2023-06-17 19:49:32 -0400

    comment command line runner2

--- src/main/java/com/munib/dash/HelloPitaDashServiceApplication.java
+++ src/main/java/com/munib/dash/HelloPitaDashServiceApplication.java
@@ -1,25 +1,10 @@
 package com.munib.dash;
 
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.munib.dash.Daos.CustomerRepository;
-import com.munib.dash.Daos.RoleDao;
-import com.munib.dash.models.CustomerModel;
-import com.munib.dash.models.Role;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class HelloPitaDashServiceApplication {
