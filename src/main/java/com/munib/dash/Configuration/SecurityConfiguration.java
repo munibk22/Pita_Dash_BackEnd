7a244abeebbcde0d93429b7605c0b2f170aeb867
munibk22 <munib22@gmail.com>
2023-06-17 20:58:36 -0400

    security update

--- src/main/java/com/munib/dash/Configuration/SecurityConfiguration.java
+++ src/main/java/com/munib/dash/Configuration/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration {
 					auth.requestMatchers("/").permitAll();
 					auth.requestMatchers("/hello").permitAll();
 					auth.requestMatchers("/payment/**").permitAll();
+					auth.requestMatchers("/auth/**").permitAll();
 					auth.requestMatchers("/admin/**").hasRole("ADMIN");
 	                auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
 					auth.anyRequest().authenticated();
