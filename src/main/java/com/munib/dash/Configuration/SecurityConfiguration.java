b2c155bc1d2e3a8371e4db430489e26f75e611b6
munibk22 <munib22@gmail.com>
2023-06-25 15:06:06 -0400

    security config4

--- src/main/java/com/munib/dash/Configuration/SecurityConfiguration.java
+++ src/main/java/com/munib/dash/Configuration/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,11 +18,12 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.filter.CorsFilter;
 
 import com.munib.dash.Services.CustomerService;
 import com.munib.dash.Utils.RSAKeyProperties;
@@ -36,44 +36,74 @@ import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 public class SecurityConfiguration {
+	
+//	 @Autowired
+//	  private CorsFilter corsFilter;
 
 	private final RSAKeyProperties keys;
 
 	public SecurityConfiguration(RSAKeyProperties keys) {
 		this.keys = keys;
 	}
-
 	
+	
+	@Bean
+	  public CorsFilter corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("https://pitadash.com");
+	    config.addAllowedOrigin("http://localhost:3000");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	  }
+	 
 	  
 	@Bean
 	public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
 		
-			http			 				
-				.authorizeHttpRequests(auth -> {
-					auth.requestMatchers("/auth/**").permitAll();
-					auth.requestMatchers("/").permitAll();
-					auth.requestMatchers("/hello").permitAll();
-					auth.requestMatchers("/payment/**").permitAll();
-					auth.requestMatchers("/auth/**").permitAll();
-					auth.requestMatchers("/customer/**").permitAll();
-					auth.requestMatchers("/admin/**").hasRole("ADMIN");
-	                auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
-					auth.anyRequest().authenticated();
-					
-				});			  
-				
-			// hhtpBasic(withDefaults()).build();
+		   http.addFilterBefore(corsFilter(), ChannelProcessingFilter.class)
+		      .authorizeHttpRequests((requests) -> requests
+		        .requestMatchers("/auth/**").permitAll()
+		        .requestMatchers("/").permitAll()
+		        .requestMatchers("/hello").permitAll()
+		        .requestMatchers("/payment/**").permitAll()
+		        .requestMatchers("/auth/**").permitAll()
+		        .requestMatchers("/customer/**").permitAll()
+		        .requestMatchers("/admin/**").hasRole("ADMIN")
+		        .requestMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+		        .anyRequest().authenticated())
+		      .headers(headers -> headers
+		        .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy", "default-src 'self'; frame-src 'self' 'unsafe-inline' 'unsafe-eval'; form-action 'self';")));
+		   		
+							
 				  http.oauth2ResourceServer()
 	                .jwt()
 	                .jwtAuthenticationConverter(jwtAuthenticationConverter());
 	        http.sessionManagement(
 	                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	            );	       	        
+	            );	       	   
+	        	       
 
 	        return http.build();   	
 
 	};
 
+	
+	 @Bean
+	  public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("https://pitadash.com","http://localhost:3000"));
+	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	  }
+
+	 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -110,15 +140,15 @@ public class SecurityConfiguration {
 	}
 
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource2() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://pitadash.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource2() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("https://pitadash.com"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
   
 
 }
