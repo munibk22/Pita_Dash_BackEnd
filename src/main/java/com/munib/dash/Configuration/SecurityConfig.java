dbb40f5bdea7bd5da001701a5eb2d511e7304f75
munibk22 <munib22@gmail.com>
2023-06-25 09:52:08 -0400

    Added Content-Security-Policy header

---
+++ src/main/java/com/munib/dash/Configuration/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.munib.dash.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers()
+            .contentTypeOptions()
+                .and()
+            .frameOptions()
+                .and()
+            .xssProtection()
+                .and()
+            .cacheControl()
+                .and()
+            .httpStrictTransportSecurity()
+                .and()
+            .addHeaderWriter((request, response) -> {
+                    response.setHeader("Content-Security-Policy", "default-src 'self'; h-ua-form-factor 'tablet';");
+            });
+    }
+}
