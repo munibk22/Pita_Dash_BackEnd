41a00bebf5c265a33a9b6481de8d06e84b779262
munibk22 <munib22@gmail.com>
2023-06-25 15:16:51 -0400

    comment corsConfig Class

--- src/main/java/com/munib/dash/Configuration/CorsConfig.java
+++ src/main/java/com/munib/dash/Configuration/CorsConfig.java
@@ -1,48 +1,48 @@
-package com.munib.dash.Configuration;
-
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-	
-	@Bean
-	  public CorsConfigurationSource corsConfigurationSource() {
-	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(Arrays.asList("*"));
-	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-	    configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", configuration);
-	    return source;
-	  }
-
-//	 @Bean
-//	    public void addCorsMappings(CorsRegistry registry) {
-//	        registry.addMapping("/**")
-//	            .allowedOrigins("https://pitadash.com")
-//	            .allowedMethods("*")
-//	            .allowedHeaders("*");
-//	    }
-	 
-	  @Bean
-	  public CorsFilter corsFilter() {
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.addAllowedOrigin("https://pitadash.com");
-	    config.addAllowedHeader("*");
-	    config.addAllowedMethod("*");
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	  }
-
-}
+//package com.munib.dash.Configuration;
+//
+//import java.util.Arrays;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//	
+//	@Bean
+//	  public CorsConfigurationSource corsConfigurationSource() {
+//	    CorsConfiguration configuration = new CorsConfiguration();
+//	    configuration.setAllowedOrigins(Arrays.asList("*"));
+//	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+//	    configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+//	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	    source.registerCorsConfiguration("/**", configuration);
+//	    return source;
+//	  }
+//
+////	 @Bean
+////	    public void addCorsMappings(CorsRegistry registry) {
+////	        registry.addMapping("/**")
+////	            .allowedOrigins("https://pitadash.com")
+////	            .allowedMethods("*")
+////	            .allowedHeaders("*");
+////	    }
+//	 
+//	  @Bean
+//	  public CorsFilter corsFilter() {
+//	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	    CorsConfiguration config = new CorsConfiguration();
+//	    config.setAllowCredentials(true);
+//	    config.addAllowedOrigin("https://pitadash.com");
+//	    config.addAllowedHeader("*");
+//	    config.addAllowedMethod("*");
+//	    source.registerCorsConfiguration("/**", config);
+//	    return new CorsFilter(source);
+//	  }
+//
+//}
