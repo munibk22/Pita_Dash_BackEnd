4cf09d9d5d14264b25fad1342af2fc1945809b34
munibk22 <munib22@gmail.com>
2023-06-25 13:39:12 -0400

    updated CRS course configuration

---
+++ src/main/java/com/munib/dash/Configuration/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.munib.dash.Configuration;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer  {
+	
+	@Bean
+	  public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("http://pitadash.com"));
+	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+	    configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	  }
+
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	            .allowedOrigins("https://pitadash.com")
+	            .allowedMethods("*")
+	            .allowedHeaders("*");
+	    }
+
+}
